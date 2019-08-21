28cfb2be311c5465617e73e3b31345c9e952ebb8
sambarnes <mistersamuelbarnes@gmail.com>
2019-08-21 17:03:50 -0500

    Updating setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from Cython.Build import cythonize, build_ext
 
 deps = {"pylxr": ["numpy", "cython"]}
 
-extensions = [Extension(name="pylxr._ext", sources=["cpylxr.pyx"])]
+extensions = [Extension(name="pylxr.cpylxr", sources=["cpylxr.pyx"])]
 
 setup(
     name="pylxr",
